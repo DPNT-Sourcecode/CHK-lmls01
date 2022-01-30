406c6d65ba4a8991db5a5bdafa55bd5df1f13fc4
sbx_user1051 <sbx_user1051@169.254.18.61>
2022-01-30 20:18:54 +0000

    Sun Jan 30 20:18:54 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,8 +7,14 @@ def checkout(skus):
     basket = defaultdict(int)
     for sku in skus:
         basket[sku] += 1
-
     print(basket)
+
+    
+
+    return 10
+
+
+checkout("AAABBCD")
 
 
 
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,5 +3,5 @@ from solutions.CHK import checkout_solution
 
 class TestSum():
     def test_checkout(self):
-        assert checkout_solution.checkout("AAABBCD") ==
-        assert sum_solution.compute(1, 2) == 3
+        assert checkout_solution.checkout("AAABBCD") == 210
+
