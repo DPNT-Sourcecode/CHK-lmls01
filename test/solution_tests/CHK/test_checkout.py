30742afa3cab86cd706d0e6613901d5a8f936bf6
sbx_user1051 <sbx_user1051@169.254.18.61>
2022-01-30 23:35:18 +0000

    Sun Jan 30 23:35:18 UTC 2022

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -13,11 +13,14 @@ class TestCheckout2():
     def test_checkout(self):
         assert checkout_solution.checkout("AAAAABBCDEEFFF") == 365
 
-
     # def test_checkout_2(self):
     #     assert checkout_solution.checkout("AAAAABBCDEEFF") == 365
+
+    def test_checkout_3(self):
+        assert checkout_solution.checkout("EE") == 80
 
     def test_invalid(self):
         assert checkout_solution.checkout("y") == -1
         assert checkout_solution.checkout("2") == -1
+
 
