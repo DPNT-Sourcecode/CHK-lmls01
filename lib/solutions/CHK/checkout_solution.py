e6d1972ed1db1fc18260a49d577db49e77d8dbb4
sbx_user1051 <sbx_user1051@169.254.18.61>
2022-01-30 22:27:49 +0000

    Sun Jan 30 22:27:49 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import pri
 
 
 # noinspection PyUnusedLocal
@@ -53,83 +54,3 @@ def calc_number_of_new_bs(basket):
         return 0
     return new_bs
 
-
-simple_prices = {
-    'A': 50,
-    'B': 30,
-    'C': 20,
-    'D': 15,
-    'E': 40,
-    'F': 10,
-    'G': 20,
-    'H': 10,
-    'I': 35,
-    'J': 60,
-    'K': 80,
-    'L': 90,
-    'M': 15,
-    'N': 40,
-    'O': 10,
-    'P': 50,
-    'Q': 30,
-    'R': 50,
-    'S': 30,
-    'T': 20,
-    'U': 40,
-    'V': 50,
-    'W': 20,
-    'X': 90,
-    'Y': 10,
-    'Z': 50,
-}
-
-bulk_prices = {
-    'A': (3, 130),
-    'B': (2, 45),
-    'F': (3, 20),
-    'H': (5, 45),
-    'K': (2, 150),
-    'P': (5, 200),
-    'Q': (3, 80),
-    'U': (4, 120),
-    'V': (2, 90),
-}
-
-double_bulk_prices = {
-    'A': (5, 200),
-    'H': (10, 80),
-    'V': (3, 130)
-}
-
-freebies = {
-    'E': (2, '')
-}
-#
-# +------+-------+------------------------+
-# | A    | 50    | 3A for 130, 5A for 200 |
-# | B    | 30    | 2B for 45              |
-# | C    | 20    |                        |
-# | D    | 15    |                        |
-# | E    | 40    | 2E get one B free      |
-# | F    | 10    | 2F get one F free      |
-# | G    | 20    |                        |
-# | H    | 10    | 5H for 45, 10H for 80  |
-# | I    | 35    |                        |
-# | J    | 60    |                        |
-# | K    | 80    | 2K for 150             |
-# | L    | 90    |                        |
-# | M    | 15    |                        |
-# | N    | 40    | 3N get one M free      |
-# | O    | 10    |                        |
-# | P    | 50    | 5P for 200             |
-# | Q    | 30    | 3Q for 80              |
-# | R    | 50    | 3R get one Q free      |
-# | S    | 30    |                        |
-# | T    | 20    |                        |
-# | U    | 40    | 3U get one U free      |
-# | V    | 50    | 2V for 90, 3V for 130  |
-# | W    | 20    |                        |
-# | X    | 90    |                        |
-# | Y    | 10    |                        |
-# | Z    | 50    |                        |
-# +------+-------+------------------------+
