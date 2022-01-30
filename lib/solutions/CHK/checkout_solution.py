1b837169f134f8260ea4618d37c5f4e2a4baffdd
sbx_user1051 <sbx_user1051@169.254.18.61>
2022-01-30 22:42:49 +0000

    Sun Jan 30 22:42:49 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,52 @@
 from collections import defaultdict
-import pri
+
+simple_prices = {
+    'A': 50,
+    'B': 30,
+    'C': 20,
+    'D': 15,
+    'E': 40,
+    'F': 10,
+    'G': 20,
+    'H': 10,
+    'I': 35,
+    'J': 60,
+    'K': 80,
+    'L': 90,
+    'M': 15,
+    'N': 40,
+    'O': 10,
+    'P': 50,
+    'Q': 30,
+    'R': 50,
+    'S': 30,
+    'T': 20,
+    'U': 40,
+    'V': 50,
+    'W': 20,
+    'X': 90,
+    'Y': 10,
+    'Z': 50,
+}
+
+bulk_prices = {
+    'A': (3, 130),
+    'B': (2, 45),
+    'F': (3, 20),
+    'H': (5, 45),
+    'K': (2, 150),
+    'P': (5, 200),
+    'Q': (3, 80),
+    'U': (4, 120),
+    'V': (2, 90),
+}
+
+double_bulk_prices = {
+    'A': (5, 200),
+    'H': (10, 80),
+    'V': (3, 130)
+}
+
 
 
 # noinspection PyUnusedLocal
@@ -16,7 +63,8 @@ def checkout(skus):
         basket[sku] += 1
 
     # modify basket with freebies
-    basket['B'] = calc_number_of_new_bs(basket)
+    modify_basket_for_freebies(basket)
+
 
     # calc sum
     basket_sum = 0
@@ -53,4 +101,23 @@ def calc_number_of_new_bs(basket):
     if new_bs < 0:
         return 0
     return new_bs
+
+
+# amends basket
+def modify_basket_for_freebies(basket):
+    freebies = {
+        'E': (2, 'B'),
+        'N': (3, 'M'),
+        'R': (3, 'Q'),
+    }
+    for qualifying_sku, val in freebies.items():
+        number_of_qualifying_skus = val[0]
+        potential_reduction_sku = val[1]
+        (number_of_max_free_items, _) = divmod(basket[qualifying_sku], number_of_qualifying_skus)
+
+        existing_skus_to_be_reduced = basket[potential_reduction_sku]
+        reduced_skus = existing_skus_to_be_reduced - number_of_max_free_items
+        if reduced_skus < 0:
+            basket[potential_reduction_sku] = 0
+        basket[potential_reduction_sku] = reduced_skus
 
